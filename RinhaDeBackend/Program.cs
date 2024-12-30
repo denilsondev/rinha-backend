7273376f15bdcee0ff6574a92280fc727a5da5d8
Denilson <denilsoncdc10@gmail.com>
2024-12-30 17:13:34 -0300

    add support ao docker

--- RinhaDeBackend/Program.cs
+++ RinhaDeBackend/Program.cs
@@ -49,10 +49,10 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Run("http://0.0.0.0:80");
